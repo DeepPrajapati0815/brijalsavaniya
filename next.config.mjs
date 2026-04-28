0c7445043f0c75a12bd8fed1501f6b1f82ba4b2c
DeepPrajapati0815 <deepprajapati0815@gmail.com>
2026-04-28 21:44:57 +0530

    fixed content policy issue

--- next.config.mjs
+++ next.config.mjs
@@ -16,7 +16,7 @@ const nextConfig = {
           { key: 'Referrer-Policy', value: 'strict-origin-when-cross-origin' },
           {
             key: 'Content-Security-Policy',
-            value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://www.instagram.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com; img-src 'self' data: https://*.instagram.com https://*.cdninstagram.com; frame-src https://www.instagram.com; connect-src 'self' https://www.googletagmanager.com;",
+            value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://www.instagram.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com; img-src 'self' data: https://*.instagram.com https://*.cdninstagram.com; frame-src https://www.instagram.com; connect-src 'self' https://www.googletagmanager.com https://brijal-cms.vercel.app http://localhost:5000;",
           },
           { key: 'Permissions-Policy', value: 'camera=(), microphone=(), geolocation=()' },
         ],
